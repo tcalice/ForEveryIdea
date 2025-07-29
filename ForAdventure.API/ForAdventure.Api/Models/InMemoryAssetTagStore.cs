3b2ede53d9567b2855f9591de39fcb577e158003
Tony Calice <tony.calice@gmail.com>
2025-07-29 01:26:33 -0400

    Delete ForAdventure.API directory

--- ForAdventure.API/ForAdventure.Api/Models/InMemoryAssetTagStore.cs
+++
@@ -1,9 +0,0 @@
-﻿using ForEveryAdventure.Api.Models;
-
-namespace ForAdventure.API.ForAdventure.Api.Models
-{
-    public class InMemoryAssetTagStore : IAssetTagStore
-    {
-        public required List<AssetTag> AssetTags { get; set; }
-    }
-}
